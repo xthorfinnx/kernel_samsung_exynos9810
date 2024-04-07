556b7cc85d8f909d9012139967985de26a478a90
Eshark22 <iollite@hotmail.com>
2024-04-07 14:44:51 +0000

    CI:Travis - Fix spacing

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#0 SMP PREEMPT Sun Apr 7 13:52:57 UTC 2024"
+#define LINUX_COMPILE_BY "eshark"
+#define LINUX_COMPILE_HOST "vivobook"
+#define LINUX_COMPILER "Ubuntu clang version 14.0.6, Ubuntu LLD 14.0.6"
---
+++ out/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.9.337-everest-ies-g69ee6fe05651-dirty"
